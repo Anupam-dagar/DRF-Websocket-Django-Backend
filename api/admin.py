313860f05f069b1780f3490ac3529f2b5f1fa539
Anupam Dagar <dagaranupam@gmail.com>
2020-03-02 21:45:36 +0800

    Create restaurent models with two tables.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Restaurent, Restaurent_data
 # Register your models here.
+
+admin.site.register(Restaurent)
+admin.site.register(Restaurent_data)
