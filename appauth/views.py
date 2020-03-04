179d2b3e0e6afeb8a3f95e2974911aca4e5c6ef3
Anupam Dagar <dagaranupam@gmail.com>
2020-03-04 19:13:22 +0800

    make register and login error more understanding. make email a required field for signup.

--- appauth/views.py
+++ appauth/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 
 from knox.models import AuthToken
@@ -20,9 +20,15 @@ class RegisterAPIView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
     def post(self, request, *args, **kwargs):
+        if request.data.get('email', None) is None:
+            return Response({
+                "error": "User email missing"
+            }, status=status.HTTP_400_BAD_REQUEST)
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
@@ -34,9 +40,15 @@ class LoginAPIView(generics.GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
+        
+        if serializer.is_valid():
+            user = serializer.validated_data
+            
+            return Response({
+                "user": UserSerializer(user, context=self.get_serializer_context()).data,
+                "token": AuthToken.objects.create(user)[1],
+            }, status=status.HTTP_200_OK)
+        
         return Response({
-            "user": UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": AuthToken.objects.create(user)[1]
-        })
+            "error": "Invalid Username of Password"
+        }, status=status.HTTP_400_BAD_REQUEST)
