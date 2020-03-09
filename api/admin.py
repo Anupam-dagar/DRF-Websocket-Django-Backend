6bada7634f2c19ab1826204dd09f43d0f7d06f06
Anupam Dagar <dagaranupam@gmail.com>
2020-03-09 20:07:41 +0800

    add database tables for collection of restaurants feature.

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Restaurant, Restaurant_names
+from .models import Restaurant, Restaurant_names, RestaurantCollections, UserCollections, Collections
 # Register your models here.
 
 admin.site.register(Restaurant)
 admin.site.register(Restaurant_names)
+admin.site.register(RestaurantCollections)
+admin.site.register(UserCollections)
+admin.site.register(Collections)
--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.postgres.fields import JSONField
+
 # Create your models here.
 
 class Restaurant_names(models.Model):
@@ -31,3 +33,38 @@ class Restaurant(models.Model):
 
     def __unicode__(self):
         return self.restaurant.name
+
+class UserCollections(models.Model):
+    # A collection created by a user
+    name = models.CharField(max_length=200, blank=False, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+
+    class Meta:
+        unique_together = ('name', 'user',)
+
+    def __str__(self):
+        return self.user.username + ' - ' + self.name
+
+    def __unicode__(self):
+        return self.user.username + ' - ' + self.name
+
+class RestaurantCollections(models.Model):
+    # All collections of restaurants by a user
+    restaurant_collection = models.ForeignKey(UserCollections, on_delete=models.CASCADE, blank=False, null=False)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, blank=False, null=False)
+
+    def __str__(self):
+        return self.restaurant_collection.name + ' - ' + self.restaurant_collection.user.username
+
+    def __unicode__(self):
+        return self.restaurant_collection.name + ' - ' + self.restaurant_collection.user.username
+
+class Collections(models.Model):
+    user_collections = models.ForeignKey(UserCollections, on_delete=models.CASCADE, blank=False, null=False)
+    collaborators = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.user_collections.name + ' - ' + self.user_collections.user.username
+
+    def __unicode__(self):
+        return self.user_collections.name + ' - ' + self.user_collections.user.username
