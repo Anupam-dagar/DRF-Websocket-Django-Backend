43ae08e9da7b59b31df19086c573cb4b630c982d
Anupam Dagar <dagaranupam@gmail.com>
2020-03-10 22:05:33 +0800

    improve API endpoint urls.

--- api/urls.py
+++ api/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('restaurants/', RestaurantListView.as_view()),
     path('restaurants/<int:pk>', RestaurantDetailView.as_view()),
     path('restaurants/<str:day>/<str:query_time>', RestaurentFilterView.as_view()),
+    path('collections/<int:user_id>', UserCollectionsCreateView.as_view()),
+    path('collections/<int:user_id>/<str:collection_name>', RestaurantCollectionsCreateView.as_view()),
     path('collections/create', UserCollectionsCreateView.as_view()),
-    path('collections/create/<int:user_id>', UserCollectionsCreateView.as_view()),
     path('collections/add', RestaurantCollectionsCreateView.as_view()),
-    path('collections/add/<int:user_id>/<str:collection_name>', RestaurantCollectionsCreateView.as_view())
 ]
