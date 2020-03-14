1fb9387b05db2d6d04bf675771abfd3f1cf175bd
Anupam Dagar <dagaranupam@gmail.com>
2020-03-14 23:10:18 +0800

    add cors for heroku frontend.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -120,7 +120,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
+    'http://localhost:3000','https://glintsfrontend.herokuapp.com'
 ]
 
 ASGI_APPLICATION = 'restaurant.routing.application'
