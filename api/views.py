cf8aa1d77812a73c7acd18d341cc825645e0755e
Anupam Dagar <dagaranupam@gmail.com>
2020-03-28 12:22:18 +0800

    fix restaurant filtering edge case of closing time.

--- api/views.py
+++ api/views.py
@@ -60,19 +60,19 @@ class RestaurentFilterView(generics.ListCreateAPIView):
         query_time = self.kwargs.get('query_time')
 
         if day == 'sunday':
-            queryset = Restaurant.objects.filter(opening_time__sunday__lte=query_time, closing_time__sunday__gte=query_time)
+            queryset = Restaurant.objects.filter(opening_time__sunday__lte=query_time, closing_time__sunday__gt=query_time)
         if day == 'monday':
-            queryset = Restaurant.objects.filter(opening_time__monday__lte=query_time, closing_time__monday__gte=query_time)
+            queryset = Restaurant.objects.filter(opening_time__monday__lte=query_time, closing_time__monday__gt=query_time)
         if day == 'tuesday':
-            queryset = Restaurant.objects.filter(opening_time__tuesday__lte=query_time, closing_time__tuesday__gte=query_time)
+            queryset = Restaurant.objects.filter(opening_time__tuesday__lte=query_time, closing_time__tuesday__gt=query_time)
         if day == 'wednesday':
-            queryset = Restaurant.objects.filter(opening_time__wednesday__lte=query_time, closing_time__wednesday__gte=query_time)
+            queryset = Restaurant.objects.filter(opening_time__wednesday__lte=query_time, closing_time__wednesday__gt=query_time)
         if day == 'thursday':
-            queryset = Restaurant.objects.filter(opening_time__thursday__lte=query_time, closing_time__thursday__gte=query_time)
+            queryset = Restaurant.objects.filter(opening_time__thursday__lte=query_time, closing_time__thursday__gt=query_time)
         if day == 'friday':
-            queryset = Restaurant.objects.filter(opening_time__friday__lte=query_time, closing_time__friday__gte=query_time)
+            queryset = Restaurant.objects.filter(opening_time__friday__lte=query_time, closing_time__friday__gt=query_time)
         if day == 'saturday':
-            queryset = Restaurant.objects.filter(opening_time__saturday__lte=query_time, closing_time__saturday__gte=query_time)
+            queryset = Restaurant.objects.filter(opening_time__saturday__lte=query_time, closing_time__saturday__gt=query_time)
 
         return queryset
     
